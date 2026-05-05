262e7d7772874c16b90b23652f2188d63ca68887
Barrionuevo Jose <bjoseonet@gmail.com>
2026-05-05 11:40:18 -0300

    TPI_2

---
+++ 007_tpi_2/notas_seleccionadas.py
@@ -0,0 +1,53 @@
+# 1. Definición del corpus (Links reales y accesibles)
+urls = [
+    # GRUPO A: Independiente / Investigación / Crítico
+
+    {
+        "url": "https://www.perfil.com/noticias/politica/el-arquitecto-que-hizo-las-refacciones-en-la-casa-del-country-de-adorni-dijo-que-no-emitio-facturas.phtml",
+        "fecha": "2026-05-05", 
+        "medio": "Perfil", 
+        "autor": "Barbara Komarovsky", 
+        "grupo": "Independiente/Investigación"
+    },
+
+    {
+        "url": "https://www.perfil.com/noticias/canal-e/fabian-waldman-puso-en-duda-en-qr-la-version-de-adorni-por-gastos-en-dolares.phtml",
+        "fecha": "2026-05-06", 
+        "medio": "Perfil", 
+        "autor": "Fabian Waldman", 
+        "grupo": "Independiente/Investigación"
+    },
+
+        {
+        "url": "https://www.pagina12.com.ar/2026/05/05/adorni-esta-en-una-cuerda-cada-vez-mas-floja/",
+        "fecha": "2026-07-05", 
+        "medio": "Pagina12", 
+        "autor": "Redacción", 
+        "grupo": "Independiente/Investigación"
+    },
+
+    # GRUPO B: Afin / Oficialista / Informativo
+
+    {
+        "url": "https://www.infobae.com/politica/2026/04/16/el-caso-adorni-sigue-sumando-capitulos-y-conspira-contra-el-intento-oficialista-de-recrear-expectativas/",
+        "fecha": "2026-04-16", 
+        "medio": "Infobae", 
+        "autor": "Eduardo Aulicino", 
+        "grupo": "Afin/Oficialista"
+    },
+
+    {
+        "url": "https://www.cronista.com/economia-politica/adorni-hablo-tras-pasar-por-el-congreso-no-voy-a-renunciar/",
+        "fecha": "2026-04-29", 
+        "medio": "El Cronista", 
+        "autor": "Redacción", 
+        "grupo": "Afin/Oficialista"
+    },
+    {
+        "url": "https://www.infobae.com/politica/2026/04/29/manuel-adorni-en-el-congreso-en-vivo-las-ultimas-noticias-sobre-el-informe-de-gestion-del-jefe-de-gabinete/",
+        "fecha": "2026-04-29", 
+        "medio": "Infobae", 
+        "autor": "David Cayón", 
+        "grupo": "Afin/Oficialista"
+    },
+]
